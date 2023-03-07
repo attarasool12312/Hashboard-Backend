e39c8f2a24c0931e13100a1ab904d38e3d0989d0
Muhammad Atif Durrani <matif.durrani@hotmail.com>
2023-03-08 02:32:30 +0500

    Code Pushed

---
+++ Hashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
